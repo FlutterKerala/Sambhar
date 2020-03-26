5105fdd5b1dd273425ee46995e5eb7116387ee10
icodelifee <mithunsmenon@gmail.com>
2020-03-26 21:51:47 +0530

    Fix Customer Sign In Page And Refractor Code

--- lib/screens/customer_login_screens.dart
+++ lib/screens/customer_login_screens.dart
@@ -3,11 +3,12 @@ import '../widgets/sing_page_top_widgets.dart';
 import '../widgets/input_feild_code_design.dart';
 
 class CustomerLoginScreens extends StatelessWidget {
-  
   final _formKey = GlobalKey<FormState>();
 
-  TextStyle headingTxt =
-      TextStyle(color: const Color.fromRGBO(94, 191, 70, 1), fontSize: 26);
+  final TextStyle headingTxt = TextStyle(
+      color: const Color.fromRGBO(94, 191, 70, 1),
+      fontSize: 26,
+      fontWeight: FontWeight.w700);
 
   @override
   Widget build(BuildContext context) {
@@ -32,51 +33,72 @@ class CustomerLoginScreens extends StatelessWidget {
                   width: screenWidth * 0.7,
                   child: Column(
                     children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        padding: EdgeInsets.all(5),
-                        decoration: textfeildDecoration,
-                        child: TextField(
-                          decoration:
-                              innerInputFeildDecoration(context, 'Username'),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        padding: EdgeInsets.all(5),
-                        decoration: textfeildDecoration,
-                        child: TextField(
-                          decoration:
-                              innerInputFeildDecoration(context, 'Password'),
-                        ),
-                      ),
+                      infoText(context),
+                      nameTextField(context),
+                      phoneNoTextField(context),
                       SizedBox(
                         height: 20,
                       ),
-                      FlatButton(
-                          onPressed: () {},
-                          child: Container(
-                              width: screenWidth * 0.7,
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 20, horizontal: 30),
-                              decoration: BoxDecoration(
-                                  color: Theme.of(context).accentColor,
-                                  borderRadius: BorderRadius.all(
-                                    const Radius.circular(5.0),
-                                  )),
-                              child: FittedBox(
-                                fit: BoxFit.fitWidth,
-                                child: Text(
-                                  'Sign up as a Seller',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                              )))
+                      signupRaisedButton(screenWidth, context)
                     ],
                   ),
                 )),
           ],
+        ),
+      ),
+    );
+  }
+
+  Text infoText(BuildContext context) {
+    return Text(
+      "Please enter your details to sign in",
+      style: TextStyle(
+          color: Theme.of(context).accentColor,
+          fontWeight: FontWeight.w500,
+          fontSize: 17),
+    );
+  }
+
+  Container nameTextField(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(5),
+      decoration: textfeildDecoration,
+      child: TextField(
+        decoration: innerInputFeildDecoration(context, 'Username'),
+      ),
+    );
+  }
+
+  Container phoneNoTextField(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(5),
+      decoration: textfeildDecoration,
+      child: TextField(
+        decoration: innerInputFeildDecoration(context, 'Password'),
+      ),
+    );
+  }
+
+  Padding signupRaisedButton(double screenWidth, BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 15),
+      child: SizedBox(
+        width: double.infinity,
+        child: RaisedButton(
+          color: Theme.of(context).accentColor,
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4))),
+          onPressed: () {},
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          child: Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.white, fontSize: 25, fontWeight: FontWeight.w500),
+          ),
         ),
       ),
     );
--- lib/widgets/sing_page_top_widgets.dart
+++ lib/widgets/sing_page_top_widgets.dart
@@ -1,29 +1,29 @@
-  import 'package:flutter/material.dart';
-  
-  Row siginPageTopWidgets(double screenWidth, TextStyle headingTxt,
-      String firstTxt, String secoundTxt) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.asset('assets/images/icons.png'),
-        Container(
-          width: screenWidth * 0.4,
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                firstTxt,
-                style: headingTxt,
-              ),
-              Text(
-                secoundTxt,
-                style: headingTxt,
-              )
-            ],
-          ),
-        )
-      ],
-    );
-  }
+import 'package:flutter/material.dart';
+
+Row siginPageTopWidgets(double screenWidth, TextStyle headingTxt,
+    String firstTxt, String secoundTxt) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Image.asset('assets/images/icons.png',width: 150,height: 150,),
+      Container(
+        width: screenWidth * 0.4,
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              firstTxt,
+              style: headingTxt,
+            ),
+            Text(
+              secoundTxt,
+              style: headingTxt,
+            )
+          ],
+        ),
+      )
+    ],
+  );
+}
